864cc1422b52df9c5b380a022bcd7eb0bfd5f0f9
Eric Swanson <eswanson@contractor.usgs.gov>
2021-07-23 14:56:36 -0400

    finished convert_file_path_folder_script.py
    Script successfully copies files from S3 folders. Also writes to csv files with files that were copied. Ignores all files but files with common image types.

--- s3_filepaths/convert_file_path_folder_script.py
+++ s3_filepaths/convert_file_path_folder_script.py
@@ -9,12 +9,11 @@ import numpy as np
 import imageio
 import calendar
 import datetime
-from dateutil import tz
-from freezegun import freeze_time
+import csv
 
 def unix2datetime(unixnumber):
     """
-   Developed from unix2dts by Chris Sherwood. Updates by Eric Swanson.
+    Developed from unix2dts by Chris Sherwood. Updates by Eric Swanson.
     Get datetime object and string (in UTC) from unix/epoch time. datetime object is "aware",
     meaning it always references a specific point in time rather than a time relative to local time.
     datetime object will be the same regardless of what timezone the function is run in.
@@ -29,9 +28,33 @@ def unix2datetime(unixnumber):
     ts = int( unixnumber[:-1]+'0')
     date_time_obj =  datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc)
     date_time_str = date_time_obj.strftime('%Y-%m-%d %H:%M:%S')
-    print("object timestamp: " + str(date_time_obj))
     return date_time_str, date_time_obj
 
+def write2csv(csv_list, csv_path):
+    """
+    Write data pertaining to the copied image files to a csv speified by the user.
+    Input:
+        csv_list - list (of lists) containing data to be written to csv. Each list includes source filepath & destination filepath
+        csv_path - desried location of generated csv file
+    Return:
+        None. However, csv file will appear in filepath the user specified.
+    """
+    
+    fieldnames = ['source filepath', 'destination filepath'] #header
+
+    #datetime info for naming csv
+    now = datetime.datetime.now()
+    now_string = now.strftime("%d-%m-%Y %H_%M_%S")
+    csv_name = csv_path + 'image copy log ' + now_string + '.csv'
+
+    #write to file
+    with open(csv_name, 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(fieldnames)
+        writer.writerows(csv_list)
+    return
+        
+    
 station = 'caco-01' #will be passed in as variable for func version of script
 
 coastcam_bucket = "s3://test-cmgp-bucket/cameras/"
@@ -44,6 +67,10 @@ image_list = fs.glob(source_folder+'/*') #caco-01 for testing
 #list of common image types
 common_image_list = ['.tif', '.tiff', '.bmp', 'jpg', '.jpeg', '.gif', '.png', '.eps', 'raw', 'cr2', '.nef', '.orf', '.sr2']
 
+#used to track copied images in a csv
+csv_path = "C:/Users/eswanson/OneDrive - DOI/Documents/GitHub/CoastCam/s3_filepaths/csv/"
+csv_list = []
+
 for image in image_list:
     for image_type in common_image_list: #loop through list of possible image types
         good_ending = False #variable to check if file ends with image type in common_image_list
@@ -53,50 +80,46 @@ for image in image_list:
     if image.endswith('.txt') or good_ending == False:
         continue #This is not an image. Skip file
     else: #this is an image
-        old_path = image.replace('test-cmgp-bucket/cameras/caco-01/products/', '') #removes but the filename so we can extract necesary info to create new url
-        print(old_path)
-        #old_path_elements = old_path.split("/") #splits up elements between forward slashes in the old filepath into a list
+        filename = image.replace('test-cmgp-bucket/cameras/caco-01/products/', '') #removes but the filename so we can extract necesary info to create new url
+        filename_elements = filename.split(".") #splits up elements of filename into a list
+        image_unix_time = filename_elements[0]
+        image_camera = filename_elements[1] 
+        image_type = filename_elements[2]
+        image_file_type = filename_elements[3]
 
-#remove empty space elements from the list
-#list will have 3 elements: "[station]", "products", "[image filename]"
-##for elements in old_path_elements:
-##	if len(elements) == 0: #if string element is ''
-##		old_path_elements.remove(elements)
-##        
-        
-    #filename = 
-##    filename_elements = filename.split(".") #splits up elements of filename into a list
-##    image_unix_time = filename_elements[0]
-##    image_camera = filename_elements[1] 
-##    image_type = filename_elements[2]
-##    image_file_type = filename_elements[3]
-##    print(filename_elements, image_unix_time, image_camera, image_type, image_file_type)
-##
-##image_date_time, date_time_obj = unix2datetime(image_unix_time) #convert unix time to date-time str in the format "yyyy-mm-dd HH:MM:SS"
-##year = image_date_time[0:4]
-##month = image_date_time[5:7]
-##day = image_date_time[8:10]
-##
-###day format for new filepath will have to be in format ddd_mmm.nn
-###use built-in python function to convert from known variables to new date
-###timetuple() method returns tuple with several date and time attributes. tm_yday is the (attribute) day of the year
-##day_of_year = str(datetime.date(int(year), int(month), int(day)).timetuple().tm_yday)
-##
-###can use built-in calendar attribute month_name[month] to get month name from a number. Month cannot have leading zeros
-##month_word = calendar.month_name[int(month)] #get full month in word form
-##month_formatted = month_word[0:3] #month in the mmm word form
-##
-##new_format_day = day_of_year + "_" + month_formatted + "." + day
-##
-##
-##new_filepath = coastcam_bucket + station + "/" + image_camera + "/" + year + "/" + new_format_day + "/raw/" #file not included
-##new_path_filename = new_filepath + filename
-##print("new filepath: " + new_path_filename)
-##
-###Use fsspec to copy image from old path to new path
-##fs.copy(source_filepath, new_path_filename)
-##
-#######destination for test is s3://test-cmgp-bucket/cameras/caco-01/c2/2019/347_Dec.13/raw/1576260000.c2.snap.jpg#####
+        image_date_time, date_time_obj = unix2datetime(image_unix_time) #convert unix time to date-time str in the format "yyyy-mm-dd HH:MM:SS"
+        year = image_date_time[0:4]
+        month = image_date_time[5:7]
+        day = image_date_time[8:10]
+
+        #day format for new filepath will have to be in format ddd_mmm.nn
+        #use built-in python function to convert from known variables to new date
+        #timetuple() method returns tuple with several date and time attributes. tm_yday is the (attribute) day of the year
+        day_of_year = str(datetime.date(int(year), int(month), int(day)).timetuple().tm_yday)
+
+        #can use built-in calendar attribute month_name[month] to get month name from a number. Month cannot have leading zeros
+        month_word = calendar.month_name[int(month)] #get full month in word form
+        month_formatted = month_word[0:3] #month in the mmm word form
+
+        new_format_day = day_of_year + "_" + month_formatted + "." + day
+
+        source_filepath = source_folder + filename
+        dest_filepath = coastcam_bucket + station + "/" + image_camera + "/" + year + "/" + new_format_day + "/raw/" + filename #file not included
+
+        #Use fsspec to copy image from old path to new path
+        fs.copy(source_filepath, dest_filepath)
+
+        csv_entry = [source_filepath, dest_filepath]
+        csv_list.append(csv_entry)
+
+#create csv file
+now = datetime.datetime.now()
+now_string = now.strftime("%d-%m-%Y %H_%M_%S")
+csv_name = 'image copy log ' + now_string + '.csv'
+write2csv(csv_list, csv_path)
+
+
+
 
 
 
