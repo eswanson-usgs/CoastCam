921d3976adc5447de51b13842545e560b9ccdeb6
Eric Swanson <eswanson@contractor.usgs.gov>
2022-02-24 14:44:02 -0500

    Update convert_day_format.py
    converts day format of folders in cmgp-coastcam S3 bucket

--- s3_filepaths/convert_day_format.py
+++ s3_filepaths/convert_day_format.py
@@ -18,7 +18,7 @@ import datetime
 ##### MAIN #####
 print("start:", datetime.datetime.now())
 #source folder filepath with format s3:/cmgp-coastcam/cameras/[station]/products/[filename]
-source_folder = "s3://test-cmgp-bucket/cameras/caco-01/" 
+source_folder = "s3://cmgp-coastcam/cameras/caco-01/" 
 
 #station caco-01 for testing
 file_system = fsspec.filesystem('s3', profile='coastcam')
@@ -29,63 +29,83 @@ for subfolder in subfolder_list:
     sub_elements = subfolder.split('/')
     if sub_elements[3].startswith('c') and len(sub_elements[3]) == 2:
         cam_list.append(subfolder)
-        
+
 for cam in cam_list:
     
     if cam.endswith('cx'):
-        
-        year_list = file_system.glob('s3://' + cam + '/merge/')
+        #2/24/22 - do nothing for now. Merge not implemented in CoastCam bucket
+        continue
+    
+##        year_list = file_system.glob('s3://' + cam + '/merge/')
+##        for year in year_list:
+##            
+##            day_list = file_system.glob('s3://' + year + '/')
+##            for day in day_list:
+##                last_element = day.split('/')[-1]
+##                if last_element == 'test':
+##                    continue #ignore. Only used for testing purposes
+##                else:
+##                    day_of_year = last_element.split('_')[0]
+##                    day_of_month = last_element.split('_')[1]
+##                    if int(day_of_year) < 10:
+##                        new_format_day = "00" + day_of_year + "_" + day_of_month
+##
+##                        day_filepath = 's3://' + day + '/'
+##                        image_list = file_system.glob(day_filepath)
+##                        for image in image_list:
+##                            filename = image.split('/')[-1]
+##                            source_filepath = 's3://' + image
+##                            destination_filepath = 's3://' + year + '/' + new_format_day + '/' + filename
+##                            file_system.copy(source_filepath, destination_filepath)
+##
+##                        
+##                    elif (int(day_of_year) >= 10) and (int(day_of_year) < 100):
+##                        new_format_day = "0" + day_of_year + "_" + day_of_month
+##
+##                        day_filepath = 's3://' + day + '/'
+##                        image_list = file_system.glob(day_filepath)
+##                        for image in image_list:
+##                            filename = image.split('/')[-1]
+##                            source_filepath = 's3://' + image
+##                            destination_filepath = 's3://' + year + '/' + new_format_day + '/' + filename
+##                            file_system.copy(source_filepath, destination_filepath)
+
+    else:
+
+        year_list = file_system.glob('s3://' + cam + '/')
         for year in year_list:
-            
+
             day_list = file_system.glob('s3://' + year + '/')
             for day in day_list:
                 last_element = day.split('/')[-1]
                 if last_element == 'test':
-                    continue #ignore. Only used for testing purposes
+                    continue #ignore. only used for testing
                 else:
                     day_of_year = last_element.split('_')[0]
                     day_of_month = last_element.split('_')[1]
-                    if int(day_of_year) < 10:
+                    if int(day_of_year) < 10 and (len(day_of_year) == 1):
                         new_format_day = "00" + day_of_year + "_" + day_of_month
 
-                        day_filepath = 's3://' + day + '/'
+                        day_filepath = 's3://' + day + '/raw/'
                         image_list = file_system.glob(day_filepath)
                         for image in image_list:
                             filename = image.split('/')[-1]
                             source_filepath = 's3://' + image
-                            destination_filepath = 's3://' + year + '/' + new_formatted_day + '/' + filename
+                            destination_filepath = 's3://' + year + '/' + new_format_day + '/raw/' + filename
                             file_system.copy(source_filepath, destination_filepath)
 
                         
-                    elif (int(day_of_year) > 10) and (int(day_of_year) < 100):
+                    elif (int(day_of_year) >= 10) and (int(day_of_year) < 100) and (len(day_of_year) == 2):
                         new_format_day = "0" + day_of_year + "_" + day_of_month
-
-                        day_filepath = 's3://' + day + '/'
+                        day_filepath = 's3://' + day + '/raw/'
                         image_list = file_system.glob(day_filepath)
                         for image in image_list:
                             filename = image.split('/')[-1]
                             source_filepath = 's3://' + image
-                            destination_filepath = 's3://' + year + '/' + new_formatted_day + '/' + filename
+                            destination_filepath = 's3://' + year + '/' + new_format_day + '/raw/' + filename
                             file_system.copy(source_filepath, destination_filepath)
 
-    else:
-        
-
-                    
-
-##    else:
-##        year_list = 
-
-
-
-###loop through folder of images
-##for image in image_list:
-##    source_filepath = "s3://" + image
-##    dest_filepath = copy_s3_image(source_filepath)
-##
-##now = datetime.datetime.now()
-##now_string = now.strftime("%d-%m-%Y %H_%M_%S")
-##print("end:", datetime.datetime.now())
+print("end:", datetime.datetime.now())
 
 
 
